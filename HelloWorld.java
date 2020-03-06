1cde95a00c26185dde9b0ba39a55cdc82a890941
gvvkumar <gollapallivvkumar@gmail.com>
2020-03-06 20:26:16 +0530

    added new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, DevOps");
     System.out.println("Hi, World");
-
+    System.out.println("dev, Ops");
       }
 }
